fe2c1a14e9fe1ac4ffa35701977c2347c9e7d9e6
Marisela Del Valle <marisela.delvalle93@gmail.com>
2015-05-06 16:54:31 -0430

    Creando excepciones

--- Tarea3.py
+++ Tarea3.py
@@ -6,35 +6,49 @@ Created on May 4, 2015
 
 class BilleteraElectronica:
     
-    def __init__(self,identificador,nombres,apellidos,CI,PIN):
-        self.id = identificador
+    def __init__(self,id,nombres,apellidos,CI,PIN,saldo,recargas,consumos):
+        self.id = id
         self.nombres = nombres
         self.apellidos = apellidos
         self.ci = CI
         self.pin = PIN
+        self.saldo = saldo
+        self.recargas = recargas
+        self.consumos = consumos
         
-    def recargas(self,monto,fecha,idEstablecimiento):
-        pass
-    
-    def consumos(self,monto,fecha,idEstablecimiento):
-        pass
-    
-    def saldo(self):
-        #return saldo
-        pass
+        if not isinstance (CI,int):
+            raise Exception ("La cedula solo debe contener caracteres numericos")
         
-    def recargar(self):
-        pass
+    def recargar(self,monto,fecha,idEstablecimiento):
+        if (monto<0) : 
+            raise Exception ("El monto a recargar no debe ser negativo")
+        else :
+            self.saldo = self.saldo + monto
+            self.recargas.append([monto,fecha,idEstablecimiento])
     
-    def consumir(self):
-        pass
+    def consumir(self,monto,fecha,idEstablecimiento,clave):
+        if (self.saldo < monto):
+            raise Exception ("El monto a consumir no debe exceder el saldo disponible")
+        elif (monto<0):
+            raise Exception ("El monto a consumir no debe ser negativo")
+        elif (self.pin!=clave): 
+            raise Exception ("La clave introducida es incorrecta")
+        else: 
+            self.saldo = self.saldo - monto
+            self.consumos.append([monto,fecha,idEstablecimiento])
+        return 0
+    
+    def verRecargas(self):
+        return self.recargas
+    
+    def verConsumos(self):
+        return self.consumos
+    
+    def verSaldo(self):
+        return self.saldo
+       
+        
         
 if __name__ == '__main__':
     pass
 
-billetera = BilleteraElectronica(2363,"Marisela","Del Valle",23638870,1245)
-print(billetera.id)
-print(billetera.nombres)
-print(billetera.apellidos)
-print(billetera.ci)
-print(billetera.pin)
